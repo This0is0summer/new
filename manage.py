6b24fe9164eac2e17e6256d2658a50dd8a482ef6
xwp <xwp_fullstack@163.com>
2018-11-30 01:12:32 +0800

    导入配置对象&使用配置对象

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 from flask import Flask
 # 导入扩展Flask_cript
 from flask_migrate import Manager
-from flask_migrate import Migrate, ManagerCommand
+from flask_migrate import Migrate
+#  ManagerCommand
 # 导入flask_sqlalchemy扩展
 from flask_sqlalchemy import SQLAlchemy
+# 导入配置对象
+from .config1 import Config
+# 导入配置对象
+
 app = Flask(__name__)
+# 使用配置对象
+app.config.from_object(Config)
 # 数据库实例
 db = SQLAlchemy(app)
 # 实例化管理对象
@@ -12,7 +19,8 @@ manage = Manager(app)
 # 使用迁移框架
 Migrate(app)
 # 通过管理器对象，添加迁移命令
-manage.add_command('db', MigrateCommand)
+# 不可以使用
+# manage.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
